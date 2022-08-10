b5ec840ce25bf92e8455bf27d328c21c26e0a598
Kris28606 <stanisavljevickristina28@gmail.com>
2022-08-11 01:26:57 +0200

    Dodata lista za prikaz svih predavaca

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Teacher } from '../model/teacher';
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherComponent implements OnInit {
 
+  teachers: Teacher[]=[];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  updateTeacher(id: number) {
+
+  }
+
+  deleteTeacher(id: number) {
+
+  }
 }
